59be670c2de396e0d0dd384a99289a587a0abae8
InterpretML <50031429+interpret-ml@users.noreply.github.com>
2020-11-10 17:25:13 -0800

    No more wildcard import for utils.
    Co-Authored-By: S Jenkins <69673492+nopdive@users.noreply.github.com>
    Co-Authored-By: Harsha-Nori <5302119+Harsha-Nori@users.noreply.github.com>

--- python/interpret-core/interpret/utils/__init__.py
+++ python/interpret-core/interpret/utils/__init__.py
@@ -1,4 +1,18 @@
 # Copyright (c) 2019 Microsoft Corporation
 # Distributed under the MIT software license
 
-from interpret.utils.all import *  # noqa: F401,F403
+# from interpret.utils.all import *  # noqa: F401,F403
+
+from interpret.utils.all import gen_name_from_class
+from interpret.utils.all import gen_global_selector
+from interpret.utils.all import gen_local_selector
+from interpret.utils.all import gen_perf_dicts
+from interpret.utils.all import unify_vector
+from interpret.utils.all import unify_data
+from interpret.utils.all import unify_predict_fn
+from interpret.utils.all import hist_per_column
+from interpret.utils.all import autogen_schema
+from interpret.utils.all import gen_feat_val_list
+from interpret.utils.all import gen_feature_names_from_df
+from interpret.utils.all import reverse_map
+from interpret.utils.all import sort_feature_value_pairs_list
