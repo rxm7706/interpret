02d1ced1d8e78365610d6f4a9ae449caf28f505b
InterpretML <50031429+interpret-ml@users.noreply.github.com>
2020-11-17 15:58:34 -0800

    Moving majority of utils to util
    Co-Authored-By: S Jenkins <69673492+nopdive@users.noreply.github.com>
    Co-Authored-By: Harsha-Nori <5302119+Harsha-Nori@users.noreply.github.com>

--- python/interpret-core/interpret/util/test/test_all.py
+++ python/interpret-core/interpret/util/test/test_all.py
@@ -1,11 +1,10 @@
-# Copyright (c) 2019 Microsoft Corporation
-# Distributed under the MIT software license
-
 import pytest
+
+from interpret.util.all import gen_perf_dicts
+from interpret.util.all import gen_feat_val_list, gen_name_from_class
+from interpret.util.all import reverse_map, unify_data, unify_vector
 import numpy as np
 import pandas as pd
-from interpret.utils import gen_feat_val_list, gen_name_from_class
-from interpret.utils import reverse_map, unify_data, unify_vector
 
 
 @pytest.fixture
@@ -79,3 +78,33 @@ def test_reverse_map():
     expected_rev_map = {1: "a", 2: "b", 3: "c"}
 
     assert actual_rev_map == expected_rev_map
+
+
+def test_gen_perf_dicts_regression():
+    y = np.array([0, 0.5])
+    scores = np.array([0.9, 0.1])
+    expected_predicted = np.array([0.9, 0.1])
+    expected_actual_score = np.array([0, 0.5])
+    expected_predicted_score = np.array([0.9, 0.1])
+
+    records = gen_perf_dicts(scores, y, False)
+    for i, di in enumerate(records):
+        assert di["actual"] == y[i]
+        assert di["predicted"] == expected_predicted[i]
+        assert di["actual_score"] == expected_actual_score[i]
+        assert di["predicted_score"] == expected_predicted_score[i]
+
+
+def test_gen_perf_dicts_classification():
+    y = np.array([0, 2])
+    scores = np.array([[0.9, 0.06, 0.04], [0.1, 0.5, 0.4],])
+    expected_predicted = np.array([0, 1])
+    expected_actual_score = np.array([0.9, 0.4])
+    expected_predicted_score = np.array([0.9, 0.5])
+
+    records = gen_perf_dicts(scores, y, True)
+    for i, di in enumerate(records):
+        assert di["actual"] == y[i]
+        assert di["predicted"] == expected_predicted[i]
+        assert di["actual_score"] == expected_actual_score[i]
+        assert di["predicted_score"] == expected_predicted_score[i]
