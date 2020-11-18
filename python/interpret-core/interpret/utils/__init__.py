2d617364549388b91233e6ca023ba40e3e3a9ede
InterpretML <50031429+interpret-ml@users.noreply.github.com>
2020-11-17 17:18:52 -0800

    Moved utils over to util yet again.
    Co-Authored-By: S Jenkins <69673492+nopdive@users.noreply.github.com>
    Co-Authored-By: Harsha-Nori <5302119+Harsha-Nori@users.noreply.github.com>

--- python/interpret-core/interpret/utils/__init__.py
+++ python/interpret-core/interpret/utils/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2019 Microsoft Corporation
 # Distributed under the MIT software license
 
-from interpret.utils.all import *  # noqa: F401,F403
+from interpret.util.all import *  # noqa: F401,F403
